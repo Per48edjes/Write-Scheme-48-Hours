df92b2b7aba03f6aa65ff8ee38ea31ebae856a8a
Ravi Dayabhai <r.i.dayabhai@gmail.com>
2023-09-05 20:29:44 -0700

    Begin working with `parsec`

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,28 @@
-module Main (module Main) where
+module Main (main) where
+
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal
+    = Atom String
+    | List [LispVal]
+    | DottedList [LispVal] LispVal
+    | Number Integer
+    | String String
+    | Bool Bool
 
 main :: IO ()
 main = do
-    putStrLn "Please enter something:"
-    userInput <- getLine
-    if null userInput
-        then do
-            putStrLn "You didn't enter anything. Please try again."
-            main -- Recursive call to keep prompting
-        else putStrLn $ "You entered: " ++ userInput
+    (expr : _) <- getArgs -- getArgs :: IO [String]
+    putStrLn (readExpr expr)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right x -> "Found value: " ++ show x
+
+spaces :: Parser ()
+spaces = skipMany1 space
